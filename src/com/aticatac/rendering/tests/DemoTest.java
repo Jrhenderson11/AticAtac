9432417169cc58e502955443818d3bf1b1194e71
David Jones <dgj636@cs.bham.ac.uk>
2018-01-24 23:33:43 +0000

    Removed merge errors

--- src/com/aticatac/rendering/tests/DemoTest.java
+++ src/com/aticatac/rendering/tests/DemoTest.java
@@ -16,56 +16,9 @@ public class DemoTest extends Application {
 		
 	}
 
-<<<<<<< HEAD
 	@Override
 	public void start(Stage stage) throws Exception {
 		DisplayComponent c1 = new DisplayComponent("assets/test/tile1.png");
-=======
-	public static void main(String[] args) {
-		JFrame window = new JFrame();
-		DisplayPanel display = new DisplayPanel(900, 600, 24);
-		try {
-			StaticComponent s1 = new StaticComponent("assets/test/tile1.png");
-			s1.setPosition(50, 50);
-			
-			AnimatedComponent a1 = new AnimatedComponent(new SpriteSheet("assets/test/spritesheet1.png", new Dimension(100, 100)), 
-					new Rectangle(200, 50, 100, 100), 0, 0, 3);
-			
-			window.setContentPane(display);
-			window.setPreferredSize(new Dimension(900, 600));
-			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
->>>>>>> branch 'rendering' of https://git-teaching.cs.bham.ac.uk/mod-team-proj-2017/aticatac
-		
-<<<<<<< HEAD
-=======
-			//Creates a new scene to display
-			Scene scene = new Scene();
-			scene.addComponent(s1); //adds components to the default layer of the scene
-			scene.addComponent(a1);
-			
-			
-			//set the display to the scene
-			display.setScene(scene);
-			
-			window.pack();
-			window.setVisible(true);
-			
-			display.start();
-			
-			while (true) {
-				s1.translate(1, 1);
-				Thread.sleep(100);
-			}
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			display.stop();
-		}
->>>>>>> branch 'rendering' of https://git-teaching.cs.bham.ac.uk/mod-team-proj-2017/aticatac
 		
 		Renderer renderer = new Renderer(900, 600);
 	
