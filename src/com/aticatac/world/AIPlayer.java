cc63fa62b6d9c8d8a414c85e254a318ecb5d1ade
lxg658 <lxg658@student.bham.ac.uk>
2018-01-25 12:07:09 +0000

    Adding basic player classes

---
+++ src/com/aticatac/world/AIPlayer.java
@@ -0,0 +1,18 @@
+package com.aticatac.world;
+
+import com.aticatac.utils.Controller;
+
+public class AIPlayer extends Player {
+	
+	public AIPlayer(Controller controller, String identifier, String colour) {
+		super(controller, identifier, colour);
+	}
+	
+	@Override
+	public char getAction() {
+		//Have some decision making here
+		return 0;
+	}
+	
+	
+}
