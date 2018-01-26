eb0d0d9e9e7bc219eb469d18e00efd63223bd1e1
David Jones <dgj636@cs.bham.ac.uk>
2018-01-26 15:19:49 +0000

    Added some comments to com.aticatac.rendering.tests.Test.java

--- src/com/aticatac/rendering/tests/Test.java
+++ src/com/aticatac/rendering/tests/Test.java
@@ -18,28 +18,29 @@ public class Test extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		DisplayComponent c1 = new DisplayComponent("assets/test/tile1.png");
+		DisplayComponent c1 = new DisplayComponent("assets/test/tile1.png"); //a component to display
 		
-		Renderer renderer = new Renderer(900, 600);
+		Renderer renderer = new Renderer(900, 600); //create a new renderer with a 900x600 display
 	
 		RenderLayer layer1 = new RenderLayer("default");
 		
-		layer1.add(c1);
+		layer1.add(c1); //add the component to this layer
 		
-		renderer.addLayer(layer1);
+		renderer.addLayer(layer1); //add this layer to the renderer
 		
-		Group root = new Group();
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
-		Canvas canvas = new Canvas(900, 600);
 		
+		Group root = new Group();      //javafx stuff
+		Scene scene = new Scene(root); 
+		stage.setScene(scene); 
+		Canvas canvas = new Canvas(900, 600); //create a canvas to allow drawing images
+		
+		//add canvas to scene
 		root.getChildren().add(canvas);
 		
+		//to render the components in the renderer, pass it the canvas GraphicsContext object
 		renderer.render(canvas.getGraphicsContext2D());
 		
-		stage.show();
-		
-		renderer.render(canvas.getGraphicsContext2D());
+		stage.show(); //display the window
 	}
 
 	public static void main(String[] args) {
