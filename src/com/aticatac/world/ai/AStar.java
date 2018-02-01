056a0c1edff9aa4bff19920a4578bfb9ba1bb9c1
lxg658 <lxg658@student.bham.ac.uk>
2018-02-01 17:02:45 +0000

    Testing basic pathfinding

--- src/com/aticatac/world/ai/AStar.java
+++ src/com/aticatac/world/ai/AStar.java
@@ -1,6 +1,7 @@
 package com.aticatac.world.ai;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,28 +16,20 @@ public class AStar {
 	private Tile finishTile;
 	private Level level;
 	private Map<Tile, Integer> cost;
-	private Map<Tile, Integer> function;
 	private Map<Tile, Tile> parent;
-	
-	
+
 	public AStar(Tile startTile, Tile finishTile, Level level) {
 		this.startTile = startTile;
 		this.finishTile = finishTile;
 		this.level = level;
 		this.cost = new HashMap<>();
-		this.function = new HashMap<>();
 		this.parent = new HashMap<>();
 	}
 
-	
-	public int getCost(Tile tile) {
-		return this.cost.get(tile);
-	}
-	
-	
 	public ArrayList<Tile> getPath() {
 		ArrayList<Tile> path = new ArrayList<>();
-		
+		cost.put(startTile, 0);
+
 		Queue<Tile> opened = new PriorityQueue<>(11, new Comparator<Tile>() {
 			@Override
 			public int compare(Tile t1, Tile t2) {
@@ -51,50 +44,60 @@ public class AStar {
 		Tile current = null;
 		ArrayList<Tile> visited = new ArrayList<Tile>();
 		opened.add(startTile);
-		
-		while(!opened.isEmpty()) {
+
+		while (!opened.isEmpty()) {
 			current = opened.poll();
-			function.put(current, current.getH(finishTile) + cost.get(current));
 			visited.add(current);
-			
-			if(current.equals(finishTile)) {
+
+			if (current.equals(finishTile)) {
 				break;
 			}
-						
+
 			for (Tile t : this.removeInvalid(getNeighbours(current))) {
 				if (!visited.contains(t)) {
-					cost.put(t, cost.get(current)+1);
+					cost.put(t, cost.get(current) + 1);
 					parent.put(t, current);
 					opened.add(t);
 				}
 			}
 		}
-		
-		assert(current.equals(finishTile));
+
+		assert (current.equals(finishTile));
 		Tile parentTile = null;
-		while(!(parentTile = parent.get(current)).equals(startTile)) {
+		
+		while (!(parentTile = parent.get(current)).equals(startTile)) {
 			path.add(current);
 			current = parentTile;
 		}
-		
-		
+		path.add(current);
+		path.add(startTile);
+		Collections.reverse(path);
+
 		return path;
 	}
 
 	private ArrayList<Tile> removeInvalid(ArrayList<Tile> neighbours) {
 		ArrayList<Tile> validNeighbours = new ArrayList<>();
-		for(Tile t: neighbours) {
-			if(this.level.getCoords(t.X, t.Y)!=1) {
+		int wall;
+		for (Tile t : neighbours) {
+			wall = this.level.getCoords(t.X, t.Y);
+			// Remove those which are off the grid or collide with a wall
+			if (wall != 1 && wall != -1) {
 				validNeighbours.add(t);
 			}
 		}
 		return validNeighbours;
 	}
-	
+
 	private ArrayList<Tile> getNeighbours(Tile t) {
 		ArrayList<Tile> neighbours = new ArrayList<>();
-		neighbours.add(new Tile(t.X, t.Y));
-		// Do this for each neighbour combination
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (i != 0 && j != 0) {
+					neighbours.add(new Tile(t.X + i, t.Y + j));
+				}
+			}
+		}
 		return neighbours;
 	}
 
