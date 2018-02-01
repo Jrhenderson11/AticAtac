38e912a4988c329aa963cdbe91098a7b64e4ad17
lxg658 <lxg658@student.bham.ac.uk>
2018-02-01 14:00:17 +0000

    Updates to player classes

--- src/com/aticatac/world/Player.java
+++ src/com/aticatac/world/Player.java
@@ -1,48 +1,59 @@
 package com.aticatac.world;
 
+import java.util.LinkedList;
+
 import com.aticatac.utils.Controller;
+import com.aticatac.utils.Tile;
 
 public class Player {
-	private final static int MINIMUM_PAINT_FOR_SHOOT = 5;
-	private final static int PAINT_DECREASE = 5;
-	private final static int BASE_PAINT_INCREASE = 5;
+	protected final static int MINIMUM_PAINT_FOR_SHOOT = 5;
+	protected final static int PAINT_DECREASE = 5;
+	protected final static int BASE_PAINT_INCREASE = 5;
+
+	protected Controller controller;
+	protected LinkedList<Tile> playerTiles;
+	protected int identifier;
+	protected String colour;
+	protected int paintLevel;
+	protected int x;
+	protected int y;
+	protected Level level;
+
+	public Player(Controller controller, Level level, int identifier, String colour) {
+		this.controller = controller;
+		this.level = level;
+		this.identifier = identifier;
+		this.colour = colour;
+	}
+
+	public void makeMovement(char control) {
+		// Send a request to renderer to move
+		// Add tile to playerTiles
+	}
+
+	public void shoot() {
+		if (this.paintLevel >= MINIMUM_PAINT_FOR_SHOOT) {
+			// Send a request to renderer to shoot
+			this.decreasePaintLevel();
+		}
+	}
+
+	public char getAction() {
+		// need to get the event and then do the necessary action
+		return 0;
+	}
 	
-    private Controller controller;
-    private int identifier;
-    private String colour;
-    private int paintLevel;
-    private int x;
-    private int y;
-    
-    public Player(Controller controller, int identifier, String colour) {
-    	this.controller = controller;
-    	this.identifier = identifier;
-    	this.colour = colour;
-    }
-        
-    public void makeMovement(char control) {
-    	// Send a request to renderer to move
-    }
-    
-    public void shoot() {
-    	if(this.paintLevel >= MINIMUM_PAINT_FOR_SHOOT) {
-    		// Send a request to renderer to shoot
-        	this.decreasePaintLevel();
-    	}
-    }
-    
-    public char getAction() {
-    	// need to get the event and then do the necessary action
-    	return 0;
-    }
-    
-    public void increasePaintLevel(int multiplier) {
-    	this.paintLevel = multiplier * BASE_PAINT_INCREASE;
-    	// increase paint depending on multiplier from possession in world
-    }
-    
-    public void decreasePaintLevel() {
-    	this.paintLevel = this.paintLevel - PAINT_DECREASE;
-    	// make a sensible decrease to paint level
-    }
+	public Tile getCurrentTile() {
+		return new Tile(this.x, this.y);
+	}
+
+	public void increasePaintLevel(int multiplier) {
+		this.paintLevel = multiplier * BASE_PAINT_INCREASE;
+		// increase paint depending on multiplier from possession in world
+	}
+
+	public void decreasePaintLevel() {
+		this.paintLevel = this.paintLevel - PAINT_DECREASE;
+		// make a sensible decrease to paint level
+	}
 }
