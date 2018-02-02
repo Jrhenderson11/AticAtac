08cb59165c19bd3ceea4ea30af83098ebaf35d72
lxg658 <lxg658@student.bham.ac.uk>
2018-02-02 15:18:24 +0000

    Updating the branch to master and making work with ai player

--- src/com/aticatac/world/World.java
+++ src/com/aticatac/world/World.java
@@ -1,15 +1,23 @@
 package com.aticatac.world;
 
 public class World {
-    private Player one;
-    private Player two;
-    private Level level;
-    
-    public World(Level level) {
-    	this.level = level;
-    }
-    
-    public Level getLevel() {
-    	return level;
-    }
+	private Player one;
+	private Player two;
+	private Level level;
+
+	int[][] map;
+
+	public World(Level level) {
+		this.level = level;
+		map = getLevel().getGrid();
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void movementHandler(int x, int y) {
+
+		 
+	}
 }
