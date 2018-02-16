405b580c2868e2491376ee0bfdde7572d9733216
David Jones <dgj636@cs.bham.ac.uk>
2018-02-16 22:39:10 +0000

    Added base classes for gun boxes

---
+++ client/src/com/aticatac/world/items/SplatGunBox.java
@@ -0,0 +1,28 @@
+package com.aticatac.world.items;
+
+import java.awt.Point;
+
+import com.aticatac.world.Player;
+
+public class SplatGunBox extends GunBox {
+	
+	/**
+	 * Creates a box that drops a SplatGun to the player when opened 
+	 * @param position The position of the top left of the collision rectangle
+	 */
+	public SplatGunBox(Point position) {
+		super(position);
+	}
+
+	/**
+	 * Opens the box and gives the weapon to the given player
+	 * @param player The Player that opened the box
+	 */
+	@Override
+	public void openBox(Player player) {
+		Gun gun = new SplatGun(player);
+		player.setGun(gun);
+		super.setState(OPENED);
+	}
+
+}
---
+++ client/src/com/aticatac/world/items/SprayGunBox.java
@@ -0,0 +1,28 @@
+package com.aticatac.world.items;
+
+import java.awt.Point;
+
+import com.aticatac.world.Player;
+
+public class SprayGunBox extends GunBox {
+
+	/**
+	 * Creates a SprayGunBox at the given position
+	 * @param position The position of the top left of the box's collision rectangle
+	 */
+	public SprayGunBox(Point position) {
+		super(position);
+	}
+
+	/**
+	 * Opens the box, giving a SprayGun to the given Player
+	 * @param player The Player who opened the box
+	 */
+	@Override
+	public void openBox(Player player) {
+		Gun gun = new SprayGun(player);
+		player.setGun(gun);
+		super.setState(OPENED);
+	}
+
+}
