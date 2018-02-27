f8850a4a2e318b0a1f2d91734bb6e28a26c92f3f
sxb1288 <sxb1288@student.bham.ac.uk>
2018-02-27 12:41:10 +0000

    changed sound from using sun to javax

--- client/src/com/aticatac/Main.java
+++ client/src/com/aticatac/Main.java
@@ -56,8 +56,9 @@ public class Main extends Application{
             public void handle(WindowEvent we) {
                 System.exit(0);
             }
-        });   
-        m.playBgMenu2();
+        });  
+        m.playBgMenu();
+        //m.playBgMenu2();
 
     }
 }
