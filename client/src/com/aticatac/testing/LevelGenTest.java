d392e6dbe1b3aa9007176f38afde0bb38e691005
lxg658 <lxg658@student.bham.ac.uk>
2018-03-20 12:51:30 +0000

    Added LevelGen Junit

--- client/src/com/aticatac/testing/LevelGenTest.java
+++ client/src/com/aticatac/testing/LevelGenTest.java
@@ -1,16 +1,49 @@
 package com.aticatac.testing;
 
+import com.aticatac.world.Level;
 import com.aticatac.world.utils.LevelGen;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class LevelGenTest {
 
-    @Test
-    public void get() {
-        if (!LevelGen.connected(LevelGen.get(100, 100))) {
-            fail("Level Generated is not connected");
-        }
-    }
+	/**
+	 * Test that the method only returns a map that is connected
+	 */
+	@Test
+	public void testGet() {
+		assertTrue(LevelGen.connected(LevelGen.get(100, 100)));
+	}
+
+	/**
+	 * Test that an empty level map is correctly identified as being connected (i.e.
+	 * there is a path between every point on the map which is not a wall and every
+	 * other point on the map which is not a wall)
+	 */
+	@Test
+	public void testConnected() {
+		Level level = new Level(100, 100);
+		assertTrue(LevelGen.connected(level.getGrid()));
+	}
+
+	/**
+	 * Test that a map where we have boxed in tiles that are not walls with walls is
+	 * correctly identified as being not connected (i.e. there is not a path between
+	 * every point on the map which is not a wall and every other point on the map
+	 * which is not a wall)
+	 */
+	@Test
+	public void testConnected2() {
+		Level level = new Level(100, 100);
+		level.makeWalls();
+		for (int i = 1; i < 7; i++) {
+			level.updateCoords(i, 6, 1);
+			level.updateCoords(6, i, 1);
+		}
+		level.updateCoords(6, 6, 1);
+		assertFalse(LevelGen.connected(level.getGrid()));
+	}
 }
